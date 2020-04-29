1ba832ec5d002bb7e80b0307575514477c76362b
Nyakaru <Nyakaru477@gmail.com>
2020-04-29 11:59:50 +0300

    feat(products): products CRUD
    - develop products CRUD and endpoints

--- movieshop/movies/urls.py
+++ movieshop/movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ProductCategoryView, SingleProductCategoryView
+from .views import ProductCategoryView, SingleProductCategoryView, ProductView, SingleProductView
 
 app_name = 'movies'
 
 urlpatterns = [
     path('categories', ProductCategoryView.as_view()),
     path('categories/<category_id>', SingleProductCategoryView.as_view()),
+    path('products', ProductView.as_view()),
+    path('products/<product_id>', SingleProductView.as_view()),
 ]
--- movieshop/movies/validators.py
+++ movieshop/movies/validators.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ValidationError
 
-from .models import ProductCategory
+from .models import ProductCategory, Products
 
 def validate_category(category_id):
     try:
         category = ProductCategory.objects.get(id=category_id)
         return category
     except ProductCategory.DoesNotExist:
-        raise ValidationError('Category id does not exist')
+        raise ValidationError('Category does not exist')
+
+def validate_product(product_id):
+    try:
+        product = Products.objects.get(id=product_id)
+        return product
+    except Products.DoesNotExist:
+        raise ValidationError('Product does not exist')
