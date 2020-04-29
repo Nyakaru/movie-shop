70ac15c47c87618810f3d74d84efe95e999b16b2
Nyakaru <Nyakaru477@gmail.com>
2020-04-29 10:59:49 +0300

    feat(product-categories): product categories CRUD
    - develop product categories CRUD endpoints

--- movieshop/movies/models.py
+++ movieshop/movies/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
--- movieshop/movieshop/settings.py
+++ movieshop/movieshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
-    'rest-framework'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
